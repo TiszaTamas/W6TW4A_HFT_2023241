93f66bac1c81723468256206600483725fa41750
TiszaTamas <tisza.tamas01@gmail.com>
2023-12-09 19:52:29 +0100

    Cleared auto created classes

--- W6TW4A_HFT_2023241.Logic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace W6TW4A_HFT_2023241.Logic
-{
-    public class Class1
-    {
-    }
-}
--- W6TW4A_HFT_2023241.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace W6TW4A_HFT_2023241.Repository
-{
-    public class Class1
-    {
-    }
-}
