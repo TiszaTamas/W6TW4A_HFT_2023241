80e4e53a5130e92d5d6bac4dd0de348094543bcd
TiszaTamas <tisza.tamas01@gmail.com>
2023-12-12 16:25:33 +0100

    Created Test classes, and configured Nu-Get Packages
    Created a Test class for each Model, and installed 3 new Nu-Get packages for testing

---
+++ W6TW4A_HFT_2023241.Test/QuestLogicTester.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W6TW4A_HFT_2023241.Test
+{
+    internal class QuestLogicTester
+    {
+    }
+}
